9168356b97ba082ff2caae921efd52366c94a783
Jack Rowsell <jack.rowsell.exmouth@gmail.com>
2024-05-31 16:43:48 +0100

    Removed Huksy as a dev dependency

--- db/connection.js
+++ db/connection.js
@@ -5,15 +5,21 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
-
 const config = {};
+
+console.log(process.env.DATABASE_URL); 
+console.log(ENV)
+
 
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE not set');
+}
+
+
 
 module.exports = new Pool(config);
