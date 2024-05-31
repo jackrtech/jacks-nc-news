5750ba5780bee7175b879c212c6663c452d15e86
Jack Rowsell <jack.rowsell.exmouth@gmail.com>
2024-05-31 11:57:39 +0100

    Completed last test in Kata 8, Refined Error Handling (Again), Refactored from mentor notes, Completed Kata 9

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,64 +1,86 @@
-const { selectArticleById, selectAllArticles, selectArticleComments, insertArticleComments, updateArticleVotes } = require('../models/articles.model')
+const { deleteComment, selectArticleById, selectAllArticles, selectArticleComments, insertArticleComments, updateArticleVotes } = require('../models/articles.model')
 
 
 exports.getArticleById = (req, res, next) => {
-    const { article_id } = req.params
-    selectArticleById(article_id).then((article) => {
-        res.status(200).send({ article })
-    }).catch((err) => {
-        next(err)
-    });
+    const { article_id } = req.params;
+    selectArticleById(article_id)
+        .then((article) => {
+            if (!article) {
+                return Promise.reject({ status: 404, msg: 'Article not found' });
+            }
+            res.status(200).send({ article });
+        })
+        .catch(next);
 };
 
 
 exports.getAllArticles = (req, res, next) => {
-    selectAllArticles().then((articles) => {
-        res.status(200).send({ articles })
-    }).catch((err) => {
-        next(err)
-    });
+    selectAllArticles()
+        .then((articles) => {
+            res.status(200).send({ articles });
+        })
+        .catch(next);
 };
+
 
 exports.getArticleComments = (req, res, next) => {
     const { article_id } = req.params;
-    selectArticleComments(article_id).then((comments) => {
-        res.status(200).send({ comments })
-    }).catch((err) => {
-        next(err)
-    });
+    selectArticleComments(article_id)
+        .then((comments) => {
+            res.status(200).send({ comments });
+        })
+        .catch(next);
 };
 
 
 exports.postArticleComments = (req, res, next) => {
     const { article_id } = req.params;
     const { username, body } = req.body;
-    //console.log(article_id, username, body)
-    const validUsernames = ['butter_bridge', 'icellusedkars', 'rogersop', 'lurker']
-    if (!validUsernames.includes(username)) {
-        return res.status(400).send({ msg: 'Invalid username' });
-    }
 
     insertArticleComments(article_id, username, body)
         .then((comment) => {
-            //console.log(comment, '<CONTROLLER')
             res.status(201).send({ comment });
-        }).catch(next);
+        })
+        .catch((err) => {
+            if (err.code === '23503' || err.code === '23505') {
+                next({ status: 404, msg: 'Article Not Found' });
+            } else {
+                next(err);
+            }
+        });
 };
 
 
 exports.patchArticleVotes = (req, res, next) => {
     const { article_id } = req.params;
     const { votes } = req.body;
-
+  
     if (typeof votes !== 'number') {
-        return res.status(400).send({ msg: 'Invalid Votes Value' });
+      return res.status(400).send({ msg: 'Invalid Votes Value' });
     }
-
-    
-
+  
     updateArticleVotes(article_id, votes)
-        .then((updatedArticle) => {
-            res.status(200).send({ article: updatedArticle });
-        })
-        .catch(next);
-};
+      .then((result) => {
+        if (result.error) {
+          res.status(result.error.status).send({ msg: result.error.msg });
+        } else {
+          res.status(200).send({ article: result });
+        }
+      })
+      .catch(next);
+  };
+
+
+  exports.deleteCommentController = (req, res, next) => {
+    const { comment_id } = req.params;
+  
+    deleteComment(comment_id)
+      .then((result) => {
+        if (result && result.error) {
+          res.status(result.error.status).send({ msg: result.error.msg });
+        } else {
+          res.status(204).send();
+        }
+      })
+      .catch(next);
+  };
--- models/articles.model.js
+++ models/articles.model.js
@@ -60,7 +60,21 @@ exports.updateArticleVotes = (article_id, inc_votes) => {
         .then((result) => {
             if (result.rows.length === 0) {
                 console.log('MODEL ERROR')
+                return { error: { status: 404, msg: 'Article Not Found' } };
             }
             return result.rows[0];
+        });
+};
+
+exports.deleteComment = (comment_id) => {
+    return db.query(`
+      DELETE FROM comments
+      WHERE comment_id = $1
+      RETURNING *;`,
+        [comment_id])
+        .then((result) => {
+            if (result.rows.length === 0) {
+                return { error: { status: 404, msg: 'Comment Not Found' } };
+            }
         });
 };
